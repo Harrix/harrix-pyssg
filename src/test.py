39dbf749f76011b62f2cbc8f137b1e7c844d9ec7
Sergienko Anton <sergienkoanton@mail.ru>
2021-03-28 19:31:34 +0300

    Add first step of creating list of html files

--- src/test.py
+++ src/test.py
@@ -2,13 +2,13 @@ import harrixpyssg as hsg
 
 
 def main():
-    pass
     # markdown_filename = 'C:/Harrix/GitHub/harrix.dev-blog-2019/2019-07-23-add-2-num-android/2019-07-23-add-2-num-android.md'
     # output_path = '../dist'
     # hsg.MarkdownToHtml(markdown_filename, output_path).start()
-    markdown_path = 'C:/Harrix/GitHub/harrix.dev-blog-2019/'
-    output_path = '../dist'
-    hsg.Dir(markdown_path, output_path).start()
+
+    markdown_path = 'C:/Harrix/GitHub/harrix.dev-blog-2017/'
+    output_path = './dist'
+    hsg.Dir_md_to_dir_html(markdown_path, output_path).start()
 
 
 if __name__ == '__main__':
