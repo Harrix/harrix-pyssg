fa915d6aba33f6f83409ced5aa81327f2dcf7c20
Sergienko Anton <sergienkoanton@mail.ru>
2021-04-04 20:15:03 +0300

    Modify. Rebuild the project structure

--- src/main.py
+++
@@ -1,9 +0,0 @@
-import harrixpyssg as hsg
-
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
---
+++ tests/main.py
@@ -0,0 +1,14 @@
+import os, sys
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+import harrixpyssg as hsg
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
