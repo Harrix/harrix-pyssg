2a272a417ce83433162124fc4f7eadf86443d217
Sergienko Anton <sergienkoanton@mail.ru>
2021-04-13 00:48:43 +0300

    Add copying of featured_image

--- harrixpyssg/markdown_to_html.py
+++ harrixpyssg/markdown_to_html.py
@@ -2,8 +2,6 @@ from pathlib import Path
 import shutil
 import markdown
 
-import re
-
 from .article import Article
 
 import harrixpylib as h
@@ -15,6 +13,7 @@ class MarkdownToHtml:
         self.output_path = Path(output_path)
         self.article = Article()
         self._dirs_of_files = ['img', 'files', 'demo', 'gallery']
+        self._featured_image_extensions = ['webp', 'jpg', 'png']
 
     def start(self):
         h.clear_directory(self.output_path)
@@ -27,8 +26,11 @@ class MarkdownToHtml:
         self.article.meta = md.Meta
         self.article.md = h.remove_yaml_from_markdown(markdown_text)
         self.article.path_html = path_html
+        self.article.html = html
 
         self.copy_dirs()
+        self.copy_featured_image()
+        # attribution
 
         h.save_file(html, self.output_path / 'index.html')
 
@@ -41,3 +43,17 @@ class MarkdownToHtml:
         if path_img.is_dir():
             shutil.copytree(path_img, self.output_path /
                             directory, dirs_exist_ok=True)
+
+    def copy_featured_image(self):
+        for ext in self._featured_image_extensions:
+            path = self.markdown_filename.parent / f'featured-image.{ext}'
+            # print(path)
+            if path.is_file():
+                shutil.copy(path, self.output_path)
+                output_path = self.output_path / f'featured-image.{ext}'
+                self.article.featured_image = output_path
+        path = self.markdown_filename.parent / 'featured-image.svg'
+        if path.is_file():
+            shutil.copy(path, self.output_path)
+            output_path = self.output_path / 'featured-image.svg'
+            self.article.featured_image_svg = output_path
