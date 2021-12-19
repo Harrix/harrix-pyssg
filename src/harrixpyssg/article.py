d80f3671639779109ccae15e279130bf121f240b
Sergienko Anton <sergienkoanton@mail.ru>
2021-12-20 00:40:09 +0300

    Update article.py

--- src/harrixpyssg/article.py
+++ src/harrixpyssg/article.py
@@ -2,6 +2,14 @@ import harrixpylib as h
 
 
 class Article:
+    """All information about an article.
+
+    Extended description of class
+
+    Attributes:
+        attr1 (int): Description of attr1
+        attr2 (str): Description of attr2
+    """
     def __init__(self):
         self.md = ""
         self.meta = dict()
