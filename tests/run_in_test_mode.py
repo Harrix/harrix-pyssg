b1cbcad5a4f7d0d5282fd2790073cf6a291caadb
Sergienko Anton <sergienkoanton@mail.ru>
2022-01-22 21:56:23 +0300

    Rename generate_from_markdown to generate_from_md

--- tests/run_in_test_mode.py
+++ tests/run_in_test_mode.py
@@ -7,7 +7,7 @@ import harrixpyssg as hsg
 def main():
     markdown_filename = './tests/data/2022-01-04-test-article/2022-01-04-test-article.md'
     output_path = './build_site'
-    a = hsg.Article().generate_from_markdown(markdown_filename, output_path)
+    a = hsg.Article().generate_from_md(markdown_filename, output_path)
 
     # markdown_path = "C:/GitHub/harrix.dev-blog-2017/"
     # output_path = './build_site'
