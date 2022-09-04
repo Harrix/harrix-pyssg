ef3f5d9f63684776d0266d6586ba3dca71560b46
Sergienko Anton <sergienkoanton@mail.ru>
2022-09-04 20:41:07 +0300

    Test generate_from_md

--- tests/run_in_test_mode.py
+++ tests/run_in_test_mode.py
@@ -5,15 +5,18 @@ import harrixpyssg as hsg
 
 
 def main():
-    h.log.is_log_file = False
     ...
-    # md_filename = './tests/data/2022-01-04-test-article/2022-01-04-test-article.md'
+    md_filename = "./tests/data/2022-01-04-test/2022-01-04-test.md"
+    html_output_folder = "./build_site"
+    a = hsg.Article().generate_from_md(md_filename, html_output_folder)
+
+    # md_filename = './tests/data/2022-01-04-test/2022-01-04-test.md'
     # html_output_folder = './build_site'
     # a = hsg.Article().generate_from_md(md_filename, html_output_folder)
 
-    md_folders = ["C:/GitHub/harrix.dev-blog-2017/"]
-    output_folder = './build_site'
-    hsg.StaticSiteGenerator(md_folders, output_folder).generate_articles()
+    # md_folders = ["C:/GitHub/harrix.dev-blog-2017/"]
+    # output_folder = './build_site'
+    # hsg.StaticSiteGenerator(md_folders, output_folder).generate_articles()
 
 
 if __name__ == "__main__":
