1c5881c19ad50c81b34fe4251da66ad19150a65f
Sergienko Anton <sergienkoanton@mail.ru>
2022-09-07 01:12:03 +0300

    Update run_in_test_mode.py

--- tests/run_in_test_mode.py
+++ tests/run_in_test_mode.py
@@ -7,13 +7,8 @@ import harrixpyssg as hsg
 def main():
     ...
     md_filename = "./tests/data/2022-01-04-test/2022-01-04-test.md"
-    html_output_folder = "./build_site"
-    a = hsg.Article().generate_from_md(md_filename, html_output_folder)
-
-    # md_filename = './tests/data/2022-01-04-test/2022-01-04-test.md'
-    # html_output_folder = './build_site'
-    # a = hsg.Article().generate_from_md(md_filename, html_output_folder)
-
+    html_folder = "./build_site"
+    a = hsg.Article().generate_from_md(md_filename, html_folder)
     # md_folders = ["C:/GitHub/harrix.dev-blog-2017/"]
     # output_folder = './build_site'
     # hsg.StaticSiteGenerator(md_folders, output_folder).generate_articles()
