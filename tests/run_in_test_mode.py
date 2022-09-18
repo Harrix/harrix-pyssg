7915dc47b7a848d0a92def7723b86eae81f2a354
Sergienko Anton <sergienkoanton@mail.ru>
2022-09-18 16:11:49 +0300

    Refactor run_in_test_mode.py

--- tests/run_in_test_mode.py
+++ tests/run_in_test_mode.py
@@ -3,16 +3,31 @@ from pathlib import Path
 import harrixpyssg as hsg
 
 
+
+
 def main():
     ...
-    # md_filename = "./tests/data/test/test.md"
-    # html_folder = "./build_site"
-    # a = hsg.Article(md_filename).generate_html(html_folder)
-    md_folder = "./tests/data"
-    # md_folder = "C:/GitHub/harrix.dev/content"
+    # run_test_article()
+    run_test_static_site_generator()
+    # run_get_yaml_tags
+
+def run_test_static_site_generator():
+    md_folder = "C:/GitHub/harrix.dev/content"
     html_folder = "./build_site"
     sg = hsg.StaticSiteGenerator(md_folder)
     sg.generate_site(html_folder)
+
+
+def run_test_article():
+    md_filename = "./tests/data/test/test.md"
+    html_folder = "./build_site"
+    hsg.Article(md_filename).generate_html(html_folder)
+
+
+def run_get_yaml_tags():
+    md_folder = "C:/GitHub/harrix.dev/content"
+    sg = hsg.StaticSiteGenerator(md_folder)
+    sg.get_yaml()
 
 
 if __name__ == "__main__":
