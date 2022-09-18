eb1cbe0e44f79860e60a62e5e1684d9a9c961ea9
Sergienko Anton <sergienkoanton@mail.ru>
2022-09-18 16:11:30 +0300

    Add test test_article__01

--- tests/tests.py
+++ tests/tests.py
@@ -1,10 +1,15 @@
 import unittest
+from pathlib import Path
 
 import harrixpyssg as hsg
 
 
 class TestHarrixpyssg(unittest.TestCase):
-    pass
+    def test_article__01(self):
+        md_filename = "./tests/data/test/test.md"
+        html_folder = "./build_site"
+        hsg.Article(md_filename).generate_html(html_folder)
+        self.assertTrue((Path(html_folder) / "index.html").is_file())
 
 
 if __name__ == "__main__":
