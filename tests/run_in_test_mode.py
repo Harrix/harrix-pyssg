9f10e5b9db48fb5e7fc3ad0c7854e90ba1682ea8
Sergienko Anton <sergienkoanton@mail.ru>
2022-11-02 21:31:32 +0300

    Update run_in_test_mode.py

--- tests/run_in_test_mode.py
+++ tests/run_in_test_mode.py
@@ -6,8 +6,8 @@ import harrixpyssg as hsg
 def main():
     ...
     # run_test()
-    # run_test_article()
-    run_test_article_meta()
+    run_test_article()
+    # run_test_article_meta()
     # run_test_static_site_generator()
     # run_get_yaml_tags()
 
@@ -16,16 +16,8 @@ def run_test():
     ...
 
 
-def run_test_static_site_generator():
-    # md_folder = "C:/GitHub/harrix.dev/content"
-    md_folder = "./tests/data"
-    html_folder = "./build_site"
-    sg = hsg.StaticSiteGenerator(md_folder)
-    sg.generate_site(html_folder)
-
-
 def run_test_article():
-    md_filename = "./tests/data/test/test.md"
+    md_filename = "./tests/data/test_01/test_01.md"
     html_folder = "./build_site"
     hsg.Article(md_filename).generate_html(html_folder)
 
@@ -34,6 +26,14 @@ def run_test_article_meta():
     md_filename = "./tests/data/test_01/test_01.md"
     html_folder = "./build_site"
     a = hsg.Article(md_filename).generate_html(html_folder)
+
+
+def run_test_static_site_generator():
+    # md_folder = "C:/GitHub/harrix.dev/content"
+    md_folder = "./tests/data"
+    html_folder = "./build_site"
+    sg = hsg.StaticSiteGenerator(md_folder)
+    sg.generate_site(html_folder)
 
 
 def run_get_yaml_tags():
