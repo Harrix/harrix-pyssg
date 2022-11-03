5d1cf64c7e686203eb131742647990131b0a7285
Sergienko Anton <sergienkoanton@mail.ru>
2022-11-03 20:51:30 +0300

    Add tests

--- tests/tests.py
+++ tests/tests.py
@@ -3,6 +3,17 @@ from pathlib import Path
 
 import harrixpyssg as hsg
 
+TEST_MD_CONTENT = """---
+date: 2022-09-18
+categories: [it, web]
+tags: [CSS]
+---
+
+# Title
+
+Hello, world!
+"""
+
 
 class TestHarrixpyssg(unittest.TestCase):
     def test_article__01(self):
@@ -10,6 +21,47 @@ class TestHarrixpyssg(unittest.TestCase):
         html_folder = "./build_site"
         hsg.Article(md_filename).generate_html(html_folder)
         self.assertTrue((Path(html_folder) / "index.html").is_file())
+
+    def test_article__02(self):
+        md_filename = "./tests/data/test_01/test_01.md"
+        a = hsg.Article(md_filename)
+        self.assertEqual(
+            len(a.md_content.splitlines()), len(TEST_MD_CONTENT.splitlines())
+        )
+
+    def test_article__03(self):
+        md_filename = "./tests/data/test_01/test_01.md"
+        a = hsg.Article(md_filename)
+        self.assertEqual(a.md_filename.name, "test_01.md")
+
+    def test_article__04(self):
+        md_filename = "./tests/data/test_01/test_01.md"
+        a = hsg.Article(md_filename)
+        a.md_without_yaml = "# New Title"
+        self.assertEqual(len(a.md_without_yaml.splitlines()), 1)
+
+    def test_article__05(self):
+        md_filename = "./tests/data/test_01/test_01.md"
+        a = hsg.Article(md_filename)
+        a.md_without_yaml = "# New Title"
+        self.assertEqual(
+            len(a.md_content.splitlines()), len(TEST_MD_CONTENT.splitlines()) - 2
+        )
+
+    def test_article__06(self):
+        md_filename = "./tests/data/test_01/test_01.md"
+        a = hsg.Article(md_filename)
+        a.md_yaml = "---\ndate: 2022-11-03\n---"
+        self.assertEqual(
+            len(a.md_content.splitlines()), len(TEST_MD_CONTENT.splitlines()) - 2
+        )
+
+    def test_article__07(self):
+        md_filename = "./tests/data/test_01/test_01.md"
+        a = hsg.Article(md_filename)
+        a.md_yaml = "---\ndate: 2022-11-03\n---"
+        a.md_without_yaml = "# New Title"
+        self.assertEqual(len(a.md_content.splitlines()), 5)
 
     def test_static_site_generator__01(self):
         md_folder = "./tests/data"
